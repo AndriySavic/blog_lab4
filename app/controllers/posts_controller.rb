d9a5d31c732b4dccc9cfec4449541061491f0d19
AndriySavic <andriy1998412@gmail.com>
2017-11-15 15:16:05 +0200

    new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show,:edit,:update,:destroy]
-  def new
-  @post = Post.new
+  
+      def new
+       @post = Post.new
   end 
 
-  def show
+      def show
     @comments = Postcomment.where(post_id: @post.id)
   end
 
-  def create
+      def create
     @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to root_path
@@ -19,21 +20,21 @@ class PostsController < ApplicationController
 
   end
   
-  def destroy
+     def destroy
     @post.destroy
     redirect_to root_path
   end
 
-  def edit
+     def edit
   end
 
-  def update
+     def update
     @post.update(post_params)
     redirect_to root_path
   end
 
   private
-    def post_params
+      def post_params
       params.require(:post).permit(:user_id,:title,:body)
   
     end
